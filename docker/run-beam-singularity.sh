79e27cc1d7e1b4ba89aeebd2dd128555ea77fd3b
tin6150 <tin@lrc>
2021-09-28 21:29:00 -0700

    beam code started inside singularity in lr6_bigmem, err out due to RO FS

--- docker/run-beam-singularity.sh
+++ docker/run-beam-singularity.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
 
+
 #SBATCH                         --job-name=SnGpuTest    # -J CLI arg will overwrite this
-#                                       CPU time (in seconds 1199 == 00:19:59 HH:MM:ss) :
-#                                       Wall clock limit in HH:MM:ss
-#SBATCH                         --time=00:59:00
+#                               CPU time (in seconds 1199 == 00:19:59 HH:MM:ss) :
+#                               Wall clock limit in HH:MM:ss
+#SBATCH                         --time=07:59:00
 
 #SBATCH                         --qos=lr_normal
-#SBATCH                         --account=scs        # -A
-#SBATCH                         --partition=lr6
+#SBATCH                         --account=scs          # -A
+#SBATCH                         --partition=lr_bigmem  # lr6
 
 
-
-#####SBATCH --time=60
 #SBATCH --nodes=1
-#####SBATCH --mem=184GB
-#SBATCH --mem=90GB
-#####SBATCH --partition=debug 
+#                              #SBATCH --mem=184GB
+#                              #SBATCH --mem=90GB   # lr6
+#SBATCH --mem=1390GB           # lr_bigmem
 
-#SBATCH                         -o  sn_%N_%j.OUT.txt
-#SBATCH                         -e  sn_%N_%j.ERR.txt
+#SBATCH                        -o  sn_%N_%j.OUT.txt
+#SBATCH                        -e  sn_%N_%j.ERR.txt
 
 
-#### sbatch script to run beam container workflow via singularity
-
+#### slurm sbatch script to run beam container workflow via singularity
+####
 #### branch: tin/singularity  (off development)
 #### will see if can run as singularity in lrc
-#### tin 2021.0927
+#### tin 2021.0928
 #### run as:
 #### sbatch  run-beam-singularity.sh 
-##XX xx test/input/beamville/beam.conf  2>&1 | tee docker_console.out
 
-MyDir=$(pwd)
+#--MyDir=$(pwd)
 
 echo "current time is"
 date
@@ -46,20 +44,17 @@ export SINGULARITY_TMPDIR="/global/scratch/tin/cacheDir"
 #// singularity pull docker://beammodel/beam:production-gemini-develop-1     
 singularity pull docker://beammodel/beam:production-gemini-develop-1  || echo "Singularity image already cached"   
 echo "**^ beyond singularity pull step ^**"
+echo "Current dir is $(pwd)"
 
 ##config=$1
 ##input_folder_name="/projects/geminixfc/repos/beam/configs/beam-run-configs/production/sfbay/gemini/" 
 ##                                                                          ^--config base
-#//input_folder_name="/global/home/users/tin/gs/tin-gh/beam/test/"
-#input_folder_name="/global/scratch/tin/tin-gh/beam/test/"
+
 input_folder_name="/global/scratch/tin/tin-gh/beam/"
 output_folder_name="$input_folder_name/output" 
+
 ##cd /projects/geminixfc/repos/beam
-
-
 #~~cd $MyDir
-##cd /global/home/users/tin/gs/tin-gh/beam/test/input 
-#XXXXcd /global/scratch/tin/tin-gh/beam/docker
 #~~cd /global/scratch/tin/tin-gh/beam/
 
 #~~input_folder_name="input"
@@ -72,8 +67,8 @@ output_folder_name="$input_folder_name/output"
 #--module load singularity-container unset XDG_RUNTIME_DIR
 
 unset LD_PRELOAD
-#export SINGULARITY_BINDPATH="$input_folder_name:/app/production/sfbay/gemini"
 export SINGULARITY_BINDPATH="$input_folder_name:/mnt"   # tmp as I don't have actual data for gemini
+#export SINGULARITY_BINDPATH="$input_folder_name:/app/production/sfbay/gemini"
 ##export SINGULARITY_BINDPATH="$input_folder_name:/apps/output"   
 # --mount source=/global/home/users/tin/tin-gh/beam/docker/test,destination=/app/test,type=bind --mount source=/global/home/users/tin/tin-gh/beam/docker/beam_output,destination=/app/output
 
@@ -82,7 +77,7 @@ export SINGULARITY_BINDPATH="$input_folder_name:/mnt"   # tmp as I don't have ac
 ##config="production/sfbay/gemini/gemini-base-2035-helics-18k.conf"
 config="production/sfbay/gemini/gemini-base-2035.conf"
 #?config="input/beamville/beam.conf"
-config="test/input/beamville/beam.conf"
+#~~config="test/input/beamville/beam.conf"
 #~~BeamBase=/global/scratch/tin/tin-gh/beam
 #~~config="test/input/beamville/beam.conf"
 
@@ -100,14 +95,18 @@ config="test/input/beamville/beam.conf"
 ## echo "staring singularity run as: SINGULARITYENV_JAVA_OPTS='-Xmx90g' singularity run --network host beam_production-gemini-develop-1.sif --config /mnt/$config"
 ## SINGULARITYENV_JAVA_OPTS='-Xmx90g' singularity run --network host beam_production-gemini-develop-1.sif --config /mnt/$config  2>&1 | tee $input_folder_name/tin.$SLURM_JOB_ID.TEE_OUT  # mostly captured by slurm  -o ...
 env | grep SINGULARITY
-#echo  $input_folder_name
+echo "config set to $config"
+echo "i/o set to $input_folder_name $output_folder_name"
+#echo  "ls -l of $input_folder_name"
 #ls -l $input_folder_name
 #echo "---ls -l /mnt next---"
 #ls -l /mnt
 
 
-echo "Starting Singularity run as: SINGULARITYENV_JAVA_OPTS='-Xmx90g' singularity run --network host beam_production-gemini-develop-1.sif --config /app/$config  2>&1 "
-SINGULARITYENV_JAVA_OPTS='-Xmx90g' singularity run --network host beam_production-gemini-develop-1.sif --config /app/$config  2>&1 
+echo "Starting Singularity run as: SINGULARITYENV_JAVA_OPTS='-Xmx1390g' singularity run --network host beam_production-gemini-develop-1.sif --config /app/$config  2>&1 "
+SINGULARITYENV_JAVA_OPTS='-Xmx1390g' singularity run --network host beam_production-gemini-develop-1.sif --config /app/$config  2>&1 
+##>> java -Xmx490g -cp /app/resources:/app/classes:/app/libs/* beam.sim.RunBeam --config /app/production/sfbay/gemini/gemini-base-2035.conf
+
 
 #//SINGULARITYENV_JAVA_OPTS='-Xmx90g' singularity shell --network host beam_production-gemini-develop-1.sif --config /app/$config
 
